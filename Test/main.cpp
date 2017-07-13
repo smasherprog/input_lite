85d9a403e81497d2b7da411039d7686470c646e6
scott <smasherprog@gmail.com>
2017-07-13 06:00:10 -0700

    adding linux implementation

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,8 +1,13 @@
 ﻿#include "Input_Lite.h"
 #include <string>
+#include <thread>
+#include <chrono>
 
+using namespace std::chrono_literals;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
+    std::this_thread::sleep_for(2s);
 
     SL::Input_Lite::SendKeyDown('H');
     SL::Input_Lite::SendKeyUp('H');
@@ -13,12 +18,12 @@ int main(int argc, char* argv[]) {
     SL::Input_Lite::SendKey('o');
     SL::Input_Lite::SendKey('o');
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::ENTER);
-    //UNICODE SUPPORT!
-    SL::Input_Lite::SendKeyDown((wchar_t)294);//the letter Ħ
-    SL::Input_Lite::SendKeyDown((wchar_t)274);//the letter Ē
-    SL::Input_Lite::SendKeyDown((wchar_t)315);//the letter Ļ
-    SL::Input_Lite::SendKeyDown((wchar_t)315);//the letter Ļ
-    SL::Input_Lite::SendKeyDown((wchar_t)526);//the letter Ȏ
+    // UNICODE SUPPORT!
+    SL::Input_Lite::SendKeyDown((wchar_t)294); // the letter Ħ
+    SL::Input_Lite::SendKeyDown((wchar_t)274); // the letter Ē
+    SL::Input_Lite::SendKeyDown((wchar_t)315); // the letter Ļ
+    SL::Input_Lite::SendKeyDown((wchar_t)315); // the letter Ļ
+    SL::Input_Lite::SendKeyDown((wchar_t)526); // the letter Ȏ
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::ENTER);
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::TAB);
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::TAB);
@@ -27,11 +32,11 @@ int main(int argc, char* argv[]) {
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::TAB);
     SL::Input_Lite::SendKeys(std::string("this is a std::string"));
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::ENTER);
-    //unicode
+    // unicode
     SL::Input_Lite::SendKeys(L"ĦĒĻĻȎ");
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::ENTER);
     SL::Input_Lite::SendKeys(std::wstring(L"ĦĒĻĻȎ"));
-    //SPECIAL KEYS
+    // SPECIAL KEYS
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::ENTER);
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::NUMPAD0);
     SL::Input_Lite::SendKey(SL::Input_Lite::SpecialKeyCodes::CAPSLOCK);
--- src/linux/Input.cpp
+++ src/linux/Input.cpp
@@ -1,27 +1,53 @@
 #include "Input_Lite.h"
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/XInput.h>
+#include <X11/extensions/XTest.h>
+#include <X11/keysym.h>
 
 namespace SL
 {
-    namespace Input_Lite
+namespace Input_Lite
+{
+    void SendKey_Impl(char key, int pressed)
     {
-
-        void SendKeyUp(char key) {
-          
+        auto display = XOpenDisplay(NULL);
+        if(key >= ' ' && key <= '~') {
+            XTestFakeKeyEvent(display, XKeysymToKeycode(display, key), pressed, CurrentTime);
+        } else {
         }
-        void SendKeyUp(wchar_t key) {
-          
-        }
-        void SendKeyDown(wchar_t key) {
-          
-        }
-        void SendKeyDown(char key) {
-         
-        }
-        void SendKeyUp(SpecialKeyCodes key) {
-            
-        }
-        void SendKeyDown(SpecialKeyCodes key) {
-            
-        }
+        XCloseDisplay(display);
     }
+    void SendKey_Impl(wchar_t key, int pressed)
+    {
+        auto display = XOpenDisplay(NULL);
+        if(key >= ' ' && key <= '~') {
+            XTestFakeKeyEvent(display, XKeysymToKeycode(display, key), pressed, CurrentTime);
+        } else {
+        }
+        XCloseDisplay(display);
+    }
+    void SendKeyUp(char key)
+    {
+        SendKey_Impl(key, False);
+    }
+    void SendKeyUp(wchar_t key)
+    {
+        SendKey_Impl(key, False);
+    }
+    void SendKeyDown(wchar_t key)
+    {
+        SendKey_Impl(key, True);
+    }
+    void SendKeyDown(char key)
+    {
+        SendKey_Impl(key, True);
+    }
+    void SendKeyUp(SpecialKeyCodes key)
+    {
+    }
+    void SendKeyDown(SpecialKeyCodes key)
+    {
+    }
+}
 }
